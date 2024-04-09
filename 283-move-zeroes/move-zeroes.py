b9ead2e5e4b30b0bf994d7e357dc94c801d67ddb
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-04-09 20:00:34 +0530

    Time: 1495 ms (6.87%) | Memory: 18.1 MB (31.48%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,24 +4,24 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        n= len(nums)
+        n = len(nums)
 
-        non_zero_arr = [0] * n
-
-        k=0
+        j = 0 
 
         for i in range(n):
 
-            if nums[i] != 0:
+            if nums[i] == 0:
 
-                non_zero_arr[k] = nums[i]
+                # Telling j to get valid non_zero_element_ind
+                j  = i + 1
 
-                k += 1
-        
-        nums[:] = non_zero_arr
+                while j < n and nums[j]==0:
 
+                    j+=1
+
+                if j>=n:
+                    break # Already zeroes are at the end
+                else:
+                    nums[i],nums[j] = nums[j], nums[i]
+            
         return nums
-        
-
-
-
