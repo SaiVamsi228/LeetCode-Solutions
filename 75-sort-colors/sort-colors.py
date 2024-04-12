dbf684b8d87279f7007f3e6d6221e7be53e00314
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-04-12 12:31:42 +0530

    Time: 37 ms (57.88%) | Memory: 16.5 MB (42.28%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,32 +4,8 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        #Dutch National Flag Algo
+        nums.sort()
 
-        n = len(nums)
-
-        low , mid, high = 0,0,n-1
-
-        while mid <= high :
-
-            if nums[mid] == 0:
-
-                nums[low],nums[mid] = nums[mid], nums[low]
-
-                low += 1
-
-                mid += 1
-            
-            elif nums[mid] == 1:
-
-                mid += 1
-            
-            elif nums[mid] == 2:
-
-                nums[mid], nums[high] = nums[high], nums[mid]
-
-                high -= 1
-        
         return nums
 
 
