72d4f799e75a18420a57f8ef03ab396be223cfe9
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-04-12 12:36:57 +0530

    Time: 32 ms (85.36%) | Memory: 16.6 MB (42.28%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,9 +4,46 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        nums.sort()
+        n = len(nums)
 
+        count_0, count_1, count_2 = 0, 0, 0
+
+        for num in nums:
+
+            if num == 0:
+
+                count_0 += 1
+            
+            if num == 1:
+
+                count_1 += 1
+            
+            if num == 2:
+
+                count_2 += 1
+        
+        for i in range(n):
+
+            if count_0:
+
+                nums[i] = 0
+
+                count_0 -= 1
+            
+            elif count_1 :
+
+                nums[i] = 1
+
+                count_1 -= 1
+            
+            else:
+
+                nums[i] = 2
+
+                count_2 -= 1
+        
         return nums
+
 
 
 
