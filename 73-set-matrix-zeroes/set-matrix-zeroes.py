63ec45a6c613c5c35c570f8c9e5a69e480ba0a32
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-04-16 17:51:27 +0530

    Time: 103 ms (56.08%) | Memory: 17.5 MB (28.33%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -6,58 +6,25 @@ class Solution:
 
         m , n = len(matrix), len(matrix[0])
 
-        first_row_zeroes = False
+        row_arr = [0]*m
+        col_arr = [0]*n
 
-        first_col_zeroes = False
-
-        for col in range(n):
-
-            if matrix[0][col] == 0:
-
-                first_row_zeroes = True
-
-                break
-        
         for row in range(m):
-
-            if matrix[row][0] == 0:
-
-                first_col_zeroes = True
-
-                break
-        
-        for row in range(1,m):
-
-            for col in range(1,n):
-
-                if matrix[row][col] == 0:
-
-                    matrix[row][0] = 0
-                    matrix[0][col] = 0
-        
-        for row in range(1,m):
-
-            for col in range(1,n):
-
-                if matrix[0][col] == 0 or matrix[row][0] == 0:
-
-                    matrix[row][col] = 0
-
-
-        if first_row_zeroes :
 
             for col in range(n):
 
-                matrix[0][col] = 0
+                if matrix[row][col] == 0:
 
-        if first_col_zeroes:
+                    row_arr[row] = 1
 
-            for row in range(m):
+                    col_arr[col] = 1
+    
 
-                matrix[row][0] = 0
-        
-        return matrix
+        for row in range(m):
 
+            for col in range(n):
 
+                if row_arr[row]==1 or col_arr[col] == 1 :
 
-
+                    matrix[row][col] = 0
+        return matrix 
