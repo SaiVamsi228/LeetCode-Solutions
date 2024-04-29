6b61bf1a06529d7a954482079e12d3bdcf44bd9b
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-04-29 11:25:59 +0530

    Time: 74 ms (68.39%) | Memory: 17.8 MB (84.43%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,27 +1,76 @@
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        left = -1
 
-        right = -1
+    def getLeft(self,nums,target):
 
         n = len(nums)
 
-        for i in range(n):
+        low, high = 0, n-1
 
-            if nums[i] == target :
+        left = -1
 
-                if left == -1:
+        while low <= high:
 
-                    left = right = i
+            mid = (low+high)//2
+
+            
+            if nums[mid] >= target:
+
+                if nums[mid] == target:
+
+                    if left == -1:
+
+                        left = mid
+                    
+                    if mid < left :
+
+                        left = mid
                 
-                if i < left :
-
-                    left = i
+                high = mid - 1
                 
-                if i > right :
+            else:
 
-                    right = i
+                low = mid + 1
         
-        return [left,right]
+        return left
 
+    def getRight(self,nums,target):
+
+        n = len(nums)
+
+        low, high = 0, n-1
+
+        right = -1
+
+        while low <= high:
+
+            mid = (low+high)//2
+
+            if nums[mid] <= target:
+
+                if nums[mid] == target:
+
+                    if right == -1:
+
+                        right = mid
+                    
+                    if mid > right :
+
+                        right = mid
+
+                    
+                low = mid + 1
+                
+            else:
+
+                high = mid - 1
+        
+        return right
+
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
+
+        left = self.getLeft(nums,target)
+
+        right = self.getRight(nums,target)
+
+        return [left,right]
