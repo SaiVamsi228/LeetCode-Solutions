4d2a20ce022eee0bb3732f29f655227dfa36e8e1
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-05-01 21:29:55 +0530

    Time: 35 ms (98.30%) | Memory: 16.7 MB (86.68%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -5,53 +5,21 @@ class Solution:
 
         n = len(nums)
 
-        if n==0:
+        if n== 1 :
+
             return 0
         
-        if n == 1:
- 
-            return 0 # If only 1 ele exists 0th ele is the peak
+        if nums[0] > nums[1]:
+
+            return 0
         
         if nums[n-1] > nums[n-2]:
 
-            return n-1 # Checking last two as it cannot be checked 
-                        # for mid + 1 if mid lies on n-1
-        
-        if nums[0] > nums[1] :
+            return n-1
 
-            return 0    # Checking first two as it cannot be checked 
-                        # for mid - 1 if mid lies on 0
-        
-        low, high = 1, n-2  # Checking leaving the edge cases
+        for i in range(1,n-1):
 
-        while low <= high :
+            if (nums[i] > nums[i-1] and nums[i] > nums[i+1]):
 
-            mid = (low + high) // 2
-
-            # If my current mid is peak return it
-
-            if  nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
-
-                return mid
-
-            # If my mid is greater than the previous then my peak
-            #  will be to the right as we are expecting a definite 
-            # ascending order as we have already looked for the 
-            # first and last elements
-            
-            if nums[mid] > nums[mid-1] :
-
-                low = mid + 1
-            
-            # If my mid is greater than the next or not greater than 
-            # previous or next then my peak will be to the left
-
-            else:
-
-                high = mid - 1
-                
-
-
-
-        
+                return i
         
