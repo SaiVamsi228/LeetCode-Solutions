27c6dc409330ce260168471e210b234ccbae51e0
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-05-01 22:09:01 +0530

    Time: 141 ms (48.95%) | Memory: 26.7 MB (63.67%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,53 +1,35 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        
-        n = len(nums)
+      
+        n = len(nums)  # Size of the array
 
-        low, high = 0, n-1
+        # Edge cases:
+        if n == 1:
+            return nums[0]
+        if nums[0] != nums[1]:
+            return nums[0]
+        if nums[n - 1] != nums[n - 2]:
+            return nums[n - 1]
 
-        while low <= high :
+        low = 1
+        high = n - 2
+        while low <= high:
+            mid = (low + high) // 2
 
-            mid = (low+high)//2
+            # If nums[mid] is the single element:
+            if nums[mid] != nums[mid + 1] and nums[mid] != nums[mid - 1]:
+                return nums[mid]
 
-            if mid%2 == 0:
-
-                if mid-1 >=0 :
-
-                    if nums[mid-1] != nums[mid]: # if there is not no problem in the left then move right
-
-                        low = mid + 1
-                    
-                    else: #else move left
-
-                        high = mid - 1
-                        
-                else:
-
-                    return  nums[mid]
-
-            
+            # We are in the left:
+            if (mid % 2 == 1 and nums[mid] == nums[mid - 1]) or (mid % 2 == 0 and nums[mid] == nums[mid + 1]):
+                # Eliminate the left half:
+                low = mid + 1
+            # We are in the right:
             else:
+                # Eliminate the right half:
+                high = mid - 1
 
-                if mid-1 >= 0 :
-                    
-                    if nums[mid-1] == nums[mid]: # if there is not no problem in the left then move right
+        # Dummy return statement:
+        return -1
 
-                        low = mid + 1
-                    
-                    else: #else move left
-
-                        high = mid - 1
-                
-                else:
-
-                    return nums[mid]
-
-        if mid%2 == 1:
-
-            return nums[mid-1]
-
-        return nums[mid]
-
-
-            
 
