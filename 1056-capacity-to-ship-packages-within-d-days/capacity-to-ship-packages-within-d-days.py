09da09885399cfed9aa4f324f4a2891e9d1c1c1f
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-05-03 20:52:23 +0530

    Time: 512 ms (14.16%) | Memory: 22.4 MB (32.86%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,57 @@
+class Solution:
+
+    def getDays(self,capacity,weights,n):
+
+        days = 1
+
+        curCapacity = 0
+
+        i = 0
+
+        while i < n:
+
+            if curCapacity + weights[i] <= capacity :
+
+                curCapacity += weights[i]
+
+                i += 1
+            
+            else:
+
+                if curCapacity == 0:
+
+                    return False
+
+                days += 1 
+
+                curCapacity = 0
+
+        return days
+                
+    def shipWithinDays(self, weights: list[int], days: int) -> int:
+        
+        n = len(weights)
+
+        leastCapacity = sum(weights)
+
+        low = 1
+
+        high = leastCapacity
+
+        while low <= high :
+
+            mC = (low + high )// 2
+
+            reqDays = self.getDays(mC,weights,n) 
+
+            if reqDays and reqDays <= days :
+
+                leastCapacity = mC
+
+                high = mC - 1
+            
+            else:
+
+                low = mC + 1
+        
+        return leastCapacity
