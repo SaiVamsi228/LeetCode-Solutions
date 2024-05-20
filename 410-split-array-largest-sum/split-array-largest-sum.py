b488a8e0c1f2d0f3176beec12b6db40fd4df0e44
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-05-20 13:38:57 +0530

    Time: 41 ms (60.96%) | Memory: 16.7 MB (23.36%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -0,0 +1,43 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+
+        low = max(nums)
+
+        high = sum(nums)
+
+        while low <= high :
+
+            largSum = (low + high)//2
+
+            if self.countSubarrays(nums,largSum) <= k:
+
+                high = largSum - 1
+            
+            else:
+
+                low = largSum + 1
+
+        return low  # if the required partitions < maximum partitions possible 
+        # see in case [2,3,1,1,1,1,1] k=5
+
+    def countSubarrays(self, nums, maxSum):
+
+        subarrays = 1
+
+        subarraySum = 0
+
+        n = len(nums)
+
+        for i in range(n):
+
+            if subarraySum + nums[i] <= maxSum:
+
+                subarraySum += nums[i]
+
+            else:
+
+                subarrays += 1
+
+                subarraySum = nums[i]
+
+        return subarrays
