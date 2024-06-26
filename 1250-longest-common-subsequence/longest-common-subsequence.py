40b9bdabd5b33f77b82317e337f11d7bd9c5da58
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-06-26 20:49:00 +0530

    Time: 521 ms (47.12%) | Memory: 42.1 MB (53.27%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,23 +1,4 @@
 class Solution:
-    def recurLongestCommonSubsequence(self,m,n,X,Y, dp):
-
-        if m == 0 or n == 0:
-            
-            return 0
-        
-        if dp[m][n] != -1:
-
-            return dp[m][n]
-        
-        if X[m-1] == Y[n-1]:
-
-            dp[m][n] =  1 + self.recurLongestCommonSubsequence(m-1, n-1 , X, Y, dp)
-
-        else:
-
-            dp[m][n] = max(self.recurLongestCommonSubsequence(m-1, n, X, Y, dp) , self.recurLongestCommonSubsequence(m, n-1, X, Y, dp))
-        
-        return dp[m][n]
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
         m = len(text1)
@@ -26,4 +7,28 @@ class Solution:
 
         dp = [ [-1 for i in range(n+1)] for j in range(m+1)]
 
-        return self.recurLongestCommonSubsequence(m, n, text1, text2, dp)
+        X = text1
+
+        Y = text2
+        
+        for i in range(m+1):
+
+            dp[i][0] = 0
+        
+        for j in range(n+1):
+
+            dp[0][j] = 0
+
+        for i in range(1, m+1):
+
+            for j in range(1, n+1):
+        
+                if X[i-1] == Y[j-1]:
+
+                    dp[i][j] =  1 + dp[i-1][j-1]
+
+                else:
+
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1])
+        
+        return dp[m][n]
