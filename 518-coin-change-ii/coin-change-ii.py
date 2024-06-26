b8af79da10ec0551ce4831fed9e1e017b9eaf50a
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-06-26 19:01:55 +0530

    Time: 296 ms (39.30%) | Memory: 32.2 MB (44.10%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,34 +1,35 @@
 class Solution:
-    def memoChange(self, n, amt, coins, dp):
 
-        if n==0 or amt == 0:
-
-            if amt == 0:
-
-                return 1
-
-            if n == 0:
-
-                return 0
-        
-        if dp[n][amt] != -1:
-
-            return dp[n][amt]
-        
-        if coins[n-1] <= amt:
-
-            dp[n][amt] = self.memoChange(n, amt - coins[n-1], coins, dp) + self.memoChange(n-1, amt, coins, dp) 
-            
-        else:
-
-            dp[n][amt] = self.memoChange(n-1, amt, coins, dp)
-        
-        return dp[n][amt]
-
-    def change(self, amount: int, coins: list[int]) -> int:
+    def change(self, amt: int, coins: list[int]) -> int:
 
         n = len(coins)
 
-        dp = [ [-1 for i in range(amount+1)] for j in range(n+1)]
+        dp = [ [-1 for i in range(amt+1)] for j in range(n+1)]
+
         
-        return self.memoChange(len(coins), amount, coins, dp)
+        for i in range(n+1):
+            
+            for j in range(amt+1):
+                    
+                if i == 0:
+
+                    dp[i][j] = 0
+            
+                if j == 0:
+
+                    dp[i][j] = 1
+                
+        
+        for i in range(1, n+1):
+
+            for j in range(1,amt+1):
+
+                if coins[i-1] <= j:
+
+                    dp[i][j] = dp[i][j - coins[i-1]] + dp[i-1][j] 
+            
+                else:
+
+                    dp[i][j] = dp[i-1][j]
+        
+        return dp[n][amt]
