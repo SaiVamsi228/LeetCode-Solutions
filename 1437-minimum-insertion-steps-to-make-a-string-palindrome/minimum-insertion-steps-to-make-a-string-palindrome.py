462bdbfa0ac1a5215c5d30ea62aa3d032312675a
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-06-28 18:12:12 +0530

    Time: 708 ms (28.17%) | Memory: 18.6 MB (49.85%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,38 @@
+class Solution:
+    
+    def LCS(self, s1, s2, n):
+        
+        dp = [ [-1 for i in range(n+1)] for j in range(n+1) ]
+        
+        for i in range(n):
+            
+            dp[0][i] = 0
+            
+            dp[i][0] = 0
+        
+        
+        for i in range(1,n+1):
+            
+            for j in range(1,n+1):
+                
+                if s1[i-1] == s2[j-1]:
+                    
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                
+                else:
+                    
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    
+        
+        return dp[n][n]
+        
+    def LPS(self,s1, n):
+        
+        return self.LCS(s1, s1[::-1], n)
+    
+    def minInsertions(self, s: str) -> int:
+
+        n = len(s)
+
+        return n - self.LPS(s, n)
+       
