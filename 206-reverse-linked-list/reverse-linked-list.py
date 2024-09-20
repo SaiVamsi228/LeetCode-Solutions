c5ef5c4a764e785b9f5f589a3d2b189fc6b42a93
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-09-20 14:33:32 +0530

    Time: 40 ms (37.08%) | Memory: 17.8 MB (5.64%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -4,28 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-
-    def revList(self,head):
-
-        if not head or not head.next:
-
-            return head
-        
-        new_head = self.revList(head.next)
-
-        front = head.next
-        
-        front.next = head
-
-        head.next = None
-
-        return new_head
-
-
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        return self.revList(head)
-        
+        stack = []
 
+        temp = head
 
+        while temp:
+
+            stack.append(temp.val)
+
+            temp = temp.next
+
+        temp = head
+
+        while stack:
+
+            temp.val = stack.pop()
+
+            temp = temp.next
         
+        return head
