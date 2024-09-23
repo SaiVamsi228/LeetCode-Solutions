a9b7a4ac02c88cc3971810249b3e3f2dccb07b56
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-09-23 10:48:26 +0530

    Time: 61 ms (45.59%) | Memory: 18.9 MB (13.66%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        freq = Counter(nums)
+
+        for k, v in freq.items():
+
+            if v == 1:
+
+                return k
+
