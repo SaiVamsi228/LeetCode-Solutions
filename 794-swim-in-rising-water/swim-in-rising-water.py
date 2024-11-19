9e88e7fdd73d9acf24fc8a1309d4526266c19326
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-11-19 22:06:48 +0530

    Time: 39 ms (34.65%) | Memory: 17.5 MB (26.76%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -1,48 +1,74 @@
-from heapq import heapify, heappush, heappop
+class DisjointSet:
+    def __init__(self, n):
+        self.rank = [0] * (n + 1)
+        self.parent = list(range(n + 1))
+        self.size = [1] * (n + 1)
+
+    def findUpar(self, node):
+        if node == self.parent[node]:
+            return node
+        self.parent[node] = self.findUpar(self.parent[node])  # Path compression
+        return self.parent[node]
+
+    def unionByRank(self, u, v):
+        ulParU = self.findUpar(u)
+        ulParV = self.findUpar(v)
+        if ulParU == ulParV:
+            return
+
+        if self.rank[ulParU] < self.rank[ulParV]:
+            self.parent[ulParU] = ulParV
+        elif self.rank[ulParU] > self.rank[ulParV]:
+            self.parent[ulParV] = ulParU
+        else:
+            self.parent[ulParV] = ulParU
+            self.rank[ulParU] += 1
+
+    def unionBySize(self, u, v):
+        ulParU = self.findUpar(u)
+        ulParV = self.findUpar(v)
+        if ulParU == ulParV:
+            return
+
+        if self.size[ulParU] < self.size[ulParV]:
+            self.parent[ulParU] = ulParV
+            self.size[ulParV] += self.size[ulParU]
+        else:
+            self.parent[ulParV] = ulParU
+            self.size[ulParU] += self.size[ulParV]
+
+
 class Solution:
-    def swimInWater(self, grid: list[list[int]]) -> int:
-        
+    def isValid(self, r, c, n):
+        return 0 <= r < n and 0 <= c < n
+
+    def swimInWater(self, grid):
         n = len(grid)
+        ds = DisjointSet(n * n)
 
-        def isValid(row, col):
+        # Store the position of each value in the grid as it is random position numbering
+        position = [[] for _ in range(n * n)]
+        for row in range(n):
+            for col in range(n):
+                position[grid[row][col]] = [row, col]
 
-            return 0 <= row < n and 0 <= col < n
+        # Directions for moving in the grid
+        dr = [-1, 0, 1, 0]
+        dc = [0, -1, 0, 1]
 
-        row = 0
+        # Process each cell in increasing order of time
+        for time in range(n * n):
+            row, col = position[time]
 
-        col = 0
+            # Check all four directions for possible paths
+            for idx in range(4):
+                nr, nc = row + dr[idx], col + dc[idx]
 
-        hp = [ (grid[0][0], row, col)]
+                if self.isValid(nr, nc, n) and grid[nr][nc] <= time:
+                    ds.unionBySize(row * n + col, nr * n + nc)
 
-        heapify(hp)
+            # Check if start and end are connected
+            if ds.findUpar(0) == ds.findUpar(n * n - 1):
+                return time
 
-        dr = [ 0, 0, 1, -1]
-
-        dc = [ 1 , -1 , 0, 0]
-
-        mini = float('inf')
-
-        time = [ [float('inf') for j in range(n)] for i in range(n)]
-
-        time[0][0] = grid[0][0]
-
-        while hp:
-
-            wait_time , row , col  = heappop(hp)
-
-            for i in range(4):
-
-                adjR, adjC = row + dr[i], col + dc[i]
-
-                if isValid(adjR, adjC) : 
-
-                    newTime = max(wait_time , grid[adjR][adjC])
-
-                    if newTime < time[adjR][adjC]:
-
-                        time[adjR][adjC] = newTime
-
-                        heappush(hp, (newTime, adjR, adjC))
-        
-
-        return time[n-1][n-1]
+        return -1
