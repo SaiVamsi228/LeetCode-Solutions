30ebbd1496bf4b67b3d320af9c09fdd90f99aba7
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-11-26 21:01:23 +0530

    Time: 1711 ms (21.88%) | Memory: 33.7 MB (31.04%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,48 +1,41 @@
 class Solution:
-    def hasSubsetSum(self, nums, Sum, n):
-
-        dp = [[ False for i in range(Sum+1)] for j in range(n+1)]
-
-        # Making all the ele of first row as False
-
-        for i in range(Sum+1):
-
-            dp[0][i] = False
-
-        # Making all the ele of first col as True
-
-        for j in range(n+1):
-
-            dp[j][0] = True
+    def isSubsetSum (self, arr, target):
+        # code here 
+        
+        n = len(arr)
+        
+        dp = [ [False for j in range(target + 1)] for i in range(n+1)]
+        
+        for j in range(target + 1): # if no ele exists
+            
+            dp[0][j] = False
+            
+        for i in range(n+1): # target 0 can definetely be made
+            
+            dp[i][0] = True
         
         for i in range(1,n+1):
-
-            for j in range(1,Sum+1):
-
-                if nums[i-1] <= j:
-
-                    dp[i][j] = dp[i-1][j - nums[i-1]] + dp[i-1][j]
+            
+            for j in range(1, target + 1):
                 
-                else:
-
+                if arr[i-1] <= j:
+                    
+                    dp[i][j] = dp[i-1][j - arr[i-1]] or dp[i-1][j]
+                
+                elif arr[i-1] > j:
+                    
                     dp[i][j] = dp[i-1][j]
         
-        return dp[n][Sum]
-
-        
+        return dp[n][target]
 
     def canPartition(self, nums: List[int]) -> bool:
         
-        Sum = sum(nums)
+        nums_sum = sum(nums)
 
-        # if my sum is odd i cant divide
-
-        if Sum % 2 == 1:
+        if nums_sum % 2 == 1:
 
             return False
         
-        halfSum = Sum // 2
+        target  = nums_sum // 2
 
-        n = len(nums)
-
-        return self.hasSubsetSum(nums, halfSum, n)
+        return self.isSubsetSum(nums, target)
