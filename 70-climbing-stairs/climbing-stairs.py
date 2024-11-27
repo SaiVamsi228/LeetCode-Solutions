8fdd406157cc58e978259f67946fd0ecf2393933
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-11-27 21:41:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (6.27%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,29 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        def solve(n):
-
-            if n == 1 :
-
-                return 1
-            
-            if n == 2:
-
-                return 2
-            
-            if dp[n] != - 1:
-
-                return dp[n]
-
-            one_step_ways = solve(n-1) 
-            
-            two_step_ways = solve(n-2)
-
-            dp[n] = one_step_ways + two_step_ways
-
-            return dp[n]
-        
         dp = [-1 for i in range(n+1)]
 
-        return solve(n)
+        if n == 1 or n == 2:
+
+            return n
+
+        dp[1] = 1
+
+        dp[2] = 2 
+
+        for i in range(3,n+1):
+
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
         
