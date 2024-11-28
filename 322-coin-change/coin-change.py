5e209f3f1dcd721305b40d56b2cbde5b6c918ca9
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-11-28 23:06:44 +0530

    Time: 1353 ms (8.55%) | Memory: 61.9 MB (5.02%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,30 +1,42 @@
 class Solution:
-    def coinChange(self, coins: list[int], amt: int) -> int:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        def getMinCoins(n, amt):
+
+            if amt == 0:
+
+                return 0
+            
+            if n == 0:
+
+                return float('inf')
+            
+            if dp[n][amt] != -1 :
+
+                return dp[n][amt]
+
+            if coins[n-1] <= amt :
+
+                take = 1 + getMinCoins(n, amt - coins[n-1])
+
+                notTake = getMinCoins(n-1, amt)
+
+                dp[n][amt] = min(take , notTake)
+            
+            else:
+
+                dp[n][amt] =  getMinCoins(n-1, amt)
+            
+            return dp[n][amt]
         
         n = len(coins)
 
-        dp = [ [-1 for i in range(amt + 1)] for j in range(n+1)]
+        dp = [ [-1 for j in range(amount+1)] for i in range(n+1)]
+
+        ans = getMinCoins(n, amount)
+
+        if ans == float('inf'):
+
+            return -1
         
-        for j in range(amt + 1): # making first row as 2**31 including 0 0 
-
-            dp[0][j] = 2**31
-
-        for i in range(1,n+1): # making first col as 0 excluding 0 0
-
-            dp[i][0] = 0               
-
-        for i in range(1, n+1):
-
-            for j in range(1, amt + 1):
-                
-                if coins[i-1] <= j:
-
-                    dp[i][j] = min( 1 + dp[i][j - coins[i-1]] , dp[i-1][j] )
-        
-                else:
-
-                    dp[i][j] = dp[i-1][j]
-        
-        ans = dp[n][amt]
-
-        return ans if ans != 2**31 else -1
+        return ans
