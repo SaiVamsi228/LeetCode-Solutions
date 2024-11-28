a5e21c7cf0f4308bac4b277168b7b21167bf738b
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-11-28 17:19:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (14.40%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,38 +1,34 @@
 class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def getMaxRobbery(nums):
 
-    def maxLoot(self,nums):
+            n = len(nums)
 
+            dp = [0 for i in range(n+1)]
+
+            dp[1] = nums[0]
+
+            if n == 1:
+
+                return dp[1]
+
+            dp[2] = max(nums[0], nums[1])
+
+            if n == 2:
+
+                return dp[2]
+
+            for i in range(3,n+1):
+
+                dp[i] = max(nums[i-1] + dp[i-2], dp[i-1])
+
+            return dp[n]
+        
         n = len(nums)
-
-        dp = [ -1 for i in range(n+1)]
-
-        dp[1] = nums[0]
 
         if n == 1:
 
-            return dp[1]
-            
-        dp[2] = max(nums[0], nums[1])
-
-        for i in range(3,n+1):
-
-            dp[i] = max(nums[i-1] + dp[(i-2)%n], dp[(i-1)%n])
-        
-        return dp[n]
-
-
-    def rob(self, nums: list[int]) -> int:
-        
-        # As the first and the last houses are connected we cant
-        # loot both houses. As our function knows to allow looting
-        # in both the houses, we send first array of houses which
-        # contains first and not last and last and not first.
-        # Reason being : Both houses can't be looted as they are adjacent.
-
-        if len(nums) == 1:
-
             return nums[0]
-
-        return max(self.maxLoot(nums[1:]), self.maxLoot(nums[:-1]))
-
-
+        # because we cant rob simulataneously
+        
+        return max(getMaxRobbery(nums[:-1]), getMaxRobbery(nums[1:]))
