cbd6c60bd15ca7becedd576d355e5eb6d88cd65b
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-12-02 18:18:03 +0530

    Time: 704 ms (33.07%) | Memory: 19.2 MB (33.03%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,41 +1,25 @@
 class Solution:
-    
-    def LCS(self, s1, s2, n):
-        
-        dp = [ [-1 for i in range(n+1)] for j in range(n+1) ]
-        
-        for i in range(n):
-            
-            dp[0][i] = 0
-            
-            dp[i][0] = 0
-        
-        
-        for i in range(1,n+1):
-            
-            for j in range(1,n+1):
-                
-                if s1[i-1] == s2[j-1]:
-                    
-                    dp[i][j] = 1 + dp[i-1][j-1]
-                
-                else:
-                    
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-                    
-        
-        return dp[n][n]
-        
-    def LPS(self,s1, n):
-        
-        return self.LCS(s1, s1[::-1], n)
-    
     def minInsertions(self, s: str) -> int:
+        def getLCS(s1,s2):
 
-        n = len(s)
+            m = len(s1)
 
-        insertions = n - self.LPS(s, n) #or deletions
+            n = len(s2)
 
-        return insertions
+            dp = [ [0 for i in range(n+1)] for j in range(m+1)] #base case initialized
+
+            for i in range(1,m+1):
+
+                for j in range(1,n+1):
+
+                    if s1[i-1] == s2[j-1]:
+
+                        dp[i][j] = 1 + dp[i-1][j-1]
+                    
+                    else:
+
+                        dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+            
+            return dp[m][n]
         
-       
+        return len(s) - getLCS(s,s[::-1]) 
