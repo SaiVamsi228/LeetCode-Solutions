5ecc16ffadd0c6a3e6f1a4cf000b10cbad8230c2
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-12-02 11:09:52 +0530

    Time: 7 ms (40.69%) | Memory: 18.4 MB (27.87%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -3,23 +3,27 @@ class Solution:
 
         rows = len(triangle)
         
-        dp = [[float('inf') for j in range(i+1)] for i in range(rows)]
+        dp = [float('inf') for j in range(rows)] 
 
         for j in range(rows):
 
-            dp[rows - 1][j] = triangle[rows-1][j]
+            dp[j] = triangle[rows-1][j]
 
         for i in range(rows-1 -1,-1,-1):
 
+            new_dp = [float('inf') for _ in range(i+1)]
+
             for j in range(i+1):
 
-                crossRightSum = triangle[i][j] + dp[i+1][j+1]
+                crossRightSum = triangle[i][j] + dp[j+1]
 
-                bottomSum = triangle[i][j] + dp[i+1][j]
+                bottomSum = triangle[i][j] + dp[j]
             
-                dp[i][j] = min(crossRightSum, bottomSum)
+                new_dp[j] = min(crossRightSum, bottomSum)
+
+            dp = new_dp
         
-        return dp[0][0]
+        return dp[0]
         
 
 
