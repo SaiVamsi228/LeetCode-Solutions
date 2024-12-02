317d88ea3a835ffa439703211093084f4d8cf366
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-12-02 10:45:04 +0530

    Time: 19 ms (6.07%) | Memory: 19.3 MB (17.05%) - LeetSync

---
+++ 120-triangle/triangle.py
@@ -0,0 +1,58 @@
+class Solution:
+    def minimumTotal(self, triangle: list[list[int]]) -> int:
+
+        rows = len(triangle)
+        
+        def isValid(row,col):
+
+            return 0 <= row < rows and 0 <= col < row + 1
+        
+        def getMinTotal(m,n):
+
+            if m == rows - 1 :
+
+                return triangle[m][n]
+
+            mini = float('inf')
+
+            if dp[m][n] != -1 :
+
+                return dp[m][n]
+
+            # go Up
+
+            if isValid(m+1,n):
+
+                aboveSum = triangle[m][n] + getMinTotal(m+1,n)
+
+                mini = min(mini, aboveSum)
+
+            # go Cross Left 
+
+            if isValid(m+1,n+1):
+
+                crossLeftSum = triangle[m][n] + getMinTotal(m+1,n+1)
+
+                mini = min(mini, crossLeftSum)
+            
+            dp[m][n] = mini
+
+            return dp[m][n]
+        
+        dp = []
+
+        for i in range(rows):
+
+            curRow = []
+
+            for j in range(i+1):
+
+                curRow.append(-1)
+
+            dp.append(curRow)
+
+        return getMinTotal(0,0)
+        
+
+
+
