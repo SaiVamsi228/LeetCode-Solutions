a74a51f970e84322f00337c7ced5880567680095
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-12-07 17:51:15 +0530

    Time: 15 ms (6.16%) | Memory: 18.4 MB (13.75%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -3,27 +3,28 @@ class Solution:
         
         n = len(prices)
         
-        dp = [ [-1 for _ in range(n+1)] for j in range(2)]
+        dp = [ -1 for j in range(2)]
 
-        dp[0][n] = dp[1][n] = 0
+        dp[0] = dp[1] = 0
 
         
-        for ind in reversed(range(n)): # ind went from 0 to n-1 but ofc initialization is taken place for ind == n
+        for ind in reversed(range(n)): 
+
+            new_dp = [ -1 for j in range(2)]
+
             for buy in range(2):
 
                 if buy == 0 :
 
-                    ans = max(-prices[ind] + dp[1][ind+1] , 0 + dp[0][ind+1]) # buy or dont do any
+                    ans = max(-prices[ind] + dp[1] , 0 + dp[0]) # buy or dont do any
                 
                 if buy == 1:
 
-                    ans = max(prices[ind] + dp[0][ind+1] , 0 + dp[1][ind+1]) # sell or dont do any
+                    ans = max(prices[ind] + dp[0] , 0 + dp[1]) # sell or dont do any
 
-                dp[buy][ind] = ans
+                new_dp[buy] = ans
             
-        for row in dp:
+            dp = new_dp
 
-            print(row)
-
-        return dp[0][0]
+        return dp[0]
 
