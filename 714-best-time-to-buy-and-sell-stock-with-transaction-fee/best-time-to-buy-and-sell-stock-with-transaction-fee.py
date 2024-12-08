3891d67dd141c5b28d763a310ee7871561f9a14c
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-12-08 17:55:28 +0530

    Time: 264 ms (30.19%) | Memory: 25.5 MB (37.05%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -2,30 +2,25 @@ class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         
         n = len(prices)
-
-        def getMaxProfit(ind,buy):
-
-            if ind == n:
-
-                return 0
-            
-            if dp[buy][ind] != -1 :
-
-                return dp[buy][ind]
-            
-            if buy == 0 :
-
-                ans = max(-prices[ind] - fee + getMaxProfit(ind+1,1) , 0 + getMaxProfit(ind+1,0)) # buy or dont do any
-            
-            if buy == 1:
-
-                ans = max(prices[ind] + getMaxProfit(ind+1,0) , 0 + getMaxProfit(ind+1,1)) # sell or dont do any
-
-            dp[buy][ind] = ans
-
-            return ans
         
-        dp = [ [-1 for _ in range(n)] for j in range(2)]
+        dp = [ [-1 for _ in range(n+1)] for j in range(2)]
 
-        return getMaxProfit(0,0)
+        dp[0][n] = dp[1][n] = 0
+
+        
+        for ind in reversed(range(n)): # ind went from 0 to n-1 but ofc initialization is taken place for ind == n
+            for buy in range(2):
+
+                if buy == 0 :
+
+                    ans = max(-prices[ind] - fee + dp[1][ind+1] , 0 + dp[0][ind+1]) # buy or dont do any
+                
+                if buy == 1:
+
+                    ans = max(prices[ind] + dp[0][ind+1] , 0 + dp[1][ind+1]) # sell or dont do any
+
+                dp[buy][ind] = ans
+            
+
+        return dp[0][0]
 
