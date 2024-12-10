dd667e435492a2ff331d10d48b11d95ac773c269
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-12-10 18:24:37 +0530

    Time: 1841 ms (28.64%) | Memory: 17.5 MB (20.73%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -3,24 +3,16 @@ class Solution:
         
         n = len(nums)
 
-        dp = [ [-1 for prev_ind in range(n+1)] for ind in range(n+1)]
+        dp = [1 for i in range(n)]
 
-        for prev_ind in range(-1,n):
+        for curInd in range(n):
 
-            dp[n][prev_ind+1] = 0
+            for prevInd in range(curInd):
 
-        for ind in reversed(range(n)):
+                if nums[curInd] > nums[prevInd]:
 
-            for prev_ind in reversed(range(-1,ind)):
+                    newLength = 1 + dp[prevInd]
 
-                take = 0
-            
-                if prev_ind == -1 or nums[ind] > nums[prev_ind]:
+                    dp[curInd] = max(dp[curInd], newLength)
 
-                    take = 1 + dp[ind+1][ind+1]
-                
-                notTake = 0 + dp[ind+1][prev_ind+1]
-
-                dp[ind][prev_ind+1] =  max(take,notTake)
-        
-        return dp[0][0]
+        return max(dp)
