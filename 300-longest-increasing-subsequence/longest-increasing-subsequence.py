29c9766c132625a2a6a5b12dad078c9ffdfe8e61
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-12-10 21:32:04 +0530

    Time: 10 ms (81.52%) | Memory: 17.4 MB (20.73%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,18 +1,42 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
+        def bin_search(arr,num):
+        
+            left = 0
+            
+            right = len(arr) - 1
+            
+            while left <= right :
+                
+                mid = (left + right)// 2
+                
+                if num <= arr[mid]:
+                    
+                    right = mid - 1
+                
+                else:
+                    
+                    left = mid + 1
+            
+            return left
+        
+        res = [nums[0]]
+        
         n = len(nums)
-
-        dp = [1 for i in range(n)]
-
-        for curInd in range(n):
-
-            for prevInd in range(curInd):
-
-                if nums[curInd] > nums[prevInd]:
-
-                    newLength = 1 + dp[prevInd]
-
-                    dp[curInd] = max(dp[curInd], newLength)
-
-        return max(dp)
+        
+        for i in range(1,n):
+            
+            num = nums[i]
+            
+            if res == [] or res[-1] < num:
+                
+                res.append(num)
+            
+            else:
+                
+                getPos = bin_search(res,num)
+        
+                res[getPos] = num
+        
+        return len(res)
