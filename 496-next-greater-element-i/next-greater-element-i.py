6055a229b0ac01d2c918aadf53cc6a36446f9a6a
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2024-12-22 15:26:25 +0530

    Time: 19 ms (22.11%) | Memory: 17.9 MB (5.76%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,67 +1,47 @@
-from collections import defaultdict
 class Solution:
-
-    def getIndex(self,ele,nums2,n):
-
-        for i in range(n):
- 
-            if nums2[i] == ele:
-
-                return i
-
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-        n1 = len(nums1)
-
         n2 = len(nums2)
-
-        hashMap = defaultdict(int)
-
+        
+        res = [-1]*n2
+        
         stack = []
-
+        
         for i in reversed(range(n2)):
-
-            curEle = nums2[i]
-
+            
             if not stack:
-
-                hashMap[curEle] = -1 
-
-                stack.append(curEle)
-            
-            elif stack[-1] > curEle:
-
-                hashMap[curEle] = stack[-1]
-            
-            else:
-
-                while stack and stack[-1] <= curEle:
-
-                    stack.pop()
-
-                if stack:
-
-                    hashMap[curEle] = stack[-1]
-
-                else:
-
-                    hashMap[curEle] = -1
-
-            stack.append(curEle)
-
-        nge = [hashMap[x] for x in nums1]
-
-        return nge
-        
-
-
-        
-
                 
+                res[i] = -1
+            
+            elif stack and stack[-1] > nums2[i]:
+                
+                res[i] = stack[-1]
+                
+            elif stack and stack[-1] <= nums2[i]:
+                
+                while stack and stack[-1] <= nums2[i]:
                     
+                    stack.pop()
                 
+                if not stack:
+                    
+                    res[i] = -1
+                
+                else:
+                    
+                    res[i] = stack[-1]
+                
+            stack.append(nums2[i])
+        
+        n1 = len(nums1)
+        
+        final_res = [-1]*n1
 
+        for i in range(n1):
 
+            ind = nums2.index(nums1[i])
 
-
-
+            final_res[i] = res[ind]
+        
+        return final_res
+        
