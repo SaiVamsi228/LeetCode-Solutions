bcc6e6c8b49dc66366268353591570441dbe8f38
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-01-14 00:16:43 +0530

    Time: 27 ms (46.60%) | Memory: 18.4 MB (6.27%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
@@ -1,20 +1,179 @@
 class Solution:
-    def subArrayRanges(self, nums: List[int]) -> int:
-        
-        sm = 0
-        n = len(nums)
+    def getNGELInd(self, arr, n):
+
+        st = []
+
+        ngl = [-1 for i in range(n)]
+
         for i in range(n):
 
-            mx = nums[i]
+            if not st:
 
-            mn = nums[i]
+                ngl[i] = -1
 
-            for j in range(i,n):
-
-                mn = min(mn, nums[j])
-
-                mx = max(mx, nums[j])
+                st.append(i)
             
-                sm += (mx - mn)
+            elif st and arr[st[-1]] > arr[i]:
+
+                ngl[i] = st[-1]
+            
+                st.append(i)
+            
+            elif st and arr[st[-1]] <= arr[i]:
+
+                while st and arr[st[-1]] < arr[i]: # IF WE SEE DUPLICATE ALSO WE STOP TO OVERCOUNT THE RANGES
+
+                    st.pop()
+                
+                if st :
+
+                    ngl[i] = st[-1]
+                
+                else:
+
+                    ngl[i] = -1
+                
+                st.append(i)
         
-        return sm
+        return ngl
+                
+
+
+    def getNGRInd(self, arr, n):
+        st = []
+
+        ngr = [n for i in range(n)]
+
+        for i in reversed(range(n)):
+
+            if not st:
+
+                ngr[i] = n
+
+                st.append(i)
+            
+            elif st and arr[st[-1]] > arr[i]:
+
+                ngr[i] = st[-1]
+            
+                st.append(i)
+            
+            elif st and arr[st[-1]] <= arr[i]:
+
+                while st and arr[st[-1]] <= arr[i]:
+
+                    st.pop()
+                
+                if st :
+
+                    ngr[i] = st[-1]
+                
+                else:
+
+                    ngr[i] = n
+                
+                st.append(i)
+        
+        return ngr
+
+    def getNSELInd(self, arr, n):
+        st = []
+
+        nsl = [-1 for i in range(n)]
+
+        for i in range(n):
+
+            if not st:
+
+                nsl[i] = -1
+
+                st.append(i)
+            
+            elif st and arr[st[-1]] < arr[i]:
+
+                nsl[i] = st[-1]
+            
+                st.append(i)
+            
+            elif st and arr[st[-1]] >= arr[i]:
+
+                while st and arr[st[-1]] > arr[i]:
+
+                    st.pop()
+                
+                if st :
+
+                    nsl[i] = st[-1]
+                
+                else:
+
+                    nsl[i] = -1
+                
+                st.append(i)
+        
+        return nsl
+
+    def getNSRInd(self, arr, n):
+        st = []
+
+        nsr = [n for i in range(n)]
+
+        for i in reversed(range(n)):
+
+            if not st:
+
+                nsr[i] = n
+
+                st.append(i)
+            
+            elif st and arr[st[-1]] < arr[i]:
+
+                nsr[i] = st[-1]
+            
+                st.append(i)
+            
+            elif st and arr[st[-1]] >= arr[i]:
+
+                while st and arr[st[-1]] >= arr[i]:
+
+                    st.pop()
+                
+                if st :
+
+                    nsr[i] = st[-1]
+                
+                else:
+
+                    nsr[i] = n
+                
+                st.append(i)
+        
+        return nsr
+
+    def subArrayRanges(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+
+        ngl = self.getNGELInd(nums,n)
+
+        ngr = self.getNGRInd(nums,n)
+
+        nsl = self.getNSELInd(nums,n)
+
+        nsr = self.getNSRInd(nums,n)
+
+        ans = 0
+
+        for i in range(n):
+
+            largFreq = (ngr[i] - i) * ( i - ngl[i])  # -1 bcz we are counting cur number twice
+
+            smalFreq = (nsr[i] - i ) * (i - nsl[i] ) 
+
+            ans += (largFreq - smalFreq) * nums[i]
+        
+        return ans
+
+
+            
+        
