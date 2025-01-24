ac2c627dc9614e925d526046abd2a7d724917e6d
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-01-24 19:05:02 +0530

    Time: 303 ms (5.13%) | Memory: 26.9 MB (22.87%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,9 +4,11 @@ class Solution:
 
         n = len(prices)
         
-        dp = [[-float('inf') for buy in range(2)] for day in range(n+1)]
+        dp = [-float('inf') for buy in range(2)]
 
-        dp[n][0] = dp[n][1] = 0
+        dp[0] = dp[1] = 0
+
+        cur_dp = [-float('inf') for i in range(2)]
 
         for day in reversed(range(n)):
 
@@ -14,21 +16,23 @@ class Solution:
 
                 if buy == 0:
 
-                    buy_profit = -prices[day] + dp[day+1][1]
+                    buy_profit = -prices[day] + dp[1]
 
-                    buy_on_other = dp[day+1][buy]  
+                    buy_on_other = dp[buy]  
 
-                    dp[day][buy] = max(buy_profit, buy_on_other)
+                    cur_dp[buy] = max(buy_profit, buy_on_other)
                 
                 elif buy == 1:
 
                     sell_profit = prices[day] 
 
-                    sell_on_other = dp[day+1][buy]
+                    sell_on_other = dp[buy]
 
-                    dp[day][buy] = max(sell_profit, sell_on_other)
+                    cur_dp[buy] = max(sell_profit, sell_on_other)
+            
+            dp = cur_dp
 
-        return dp[0][0]
+        return dp[0]
 
 
 
