e42cb04cd92312325ab662b52767bd6612c2431b
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-01-24 17:49:49 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (8.02%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,47 +4,41 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        n = len(nums)
+        red_cnt = 0
 
-        count_0, count_1, count_2 = 0, 0, 0
+        white_cnt = 0
+
+        blue_cnt = 0
 
         for num in nums:
 
-            if num == 0:
+            if num == 0: red_cnt += 1
 
-                count_0 += 1
-            
-            if num == 1:
+            elif num == 1 : white_cnt += 1
 
-                count_1 += 1
-            
-            if num == 2:
-
-                count_2 += 1
+            else: blue_cnt += 1
         
+        n = len(nums)
+
         for i in range(n):
 
-            if count_0:
+            if red_cnt :
 
                 nums[i] = 0
 
-                count_0 -= 1
+                red_cnt -= 1
             
-            elif count_1 :
+            elif white_cnt :
 
                 nums[i] = 1
 
-                count_1 -= 1
+                white_cnt -= 1
             
             else:
 
                 nums[i] = 2
 
-                count_2 -= 1
+                blue_cnt -= 1
         
         return nums
-
-
-
-
         
