ee0cf7a60bba723462e45944b58933329baa11b2
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-01-24 21:25:57 +0530

    Time: 523 ms (5.03%) | Memory: 43.1 MB (5.58%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,45 +1,34 @@
+# TABULATION APPROACH
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+    def maxSubArray(self, nums: list[int]) -> int:
         
-        # nums = [-2,1,-3,4,-1,2,1,-5,4]
-        # o/p : 6
-
-        def getMaxSubarrSum(n,start):
-
-          if n == 1:
-
-            if start == 0:
-
-                return nums[0]
-            
-            else:
-                
-                return max(0,nums[0])
-
-          if dp[n][start] != -float('inf'):
-
-            return dp[n][start]
-
-          if start == 0:
-
-            start_new_sub_arr = nums[n-1] + getMaxSubarrSum(n-1,1)
-
-            start_at_other = getMaxSubarrSum(n-1,start)
-
-            dp[n][start] = max(start_new_sub_arr, start_at_other)
-          
-          elif start == 1:
-
-            end_cur_sub_arr = 0
-            
-            end_at_other = nums[n-1] + getMaxSubarrSum(n-1,start)
-
-            dp[n][start] = max(end_cur_sub_arr, end_at_other)
-          
-          return dp[n][start]
-
         n = len(nums)
 
         dp = [[-float('inf') for start in range(2)] for ind in range(n+1)]
+        
+        dp[1][0] = nums[0]
 
-        return getMaxSubarrSum(n,0)
+        dp[1][1] = max(0,nums[0])
+
+        for ind in range(2,n+1):
+
+            for start in range(2):
+
+                if start == 0:
+
+                    start_new_sub_arr = nums[ind-1] + dp[ind-1][1]
+
+                    start_at_other = dp[ind-1][start]
+
+                    dp[ind][start] = max(start_new_sub_arr, start_at_other)
+                
+                elif start == 1:
+
+                    end_cur_sub_arr = 0
+                    
+                    end_at_other = nums[ind-1] + dp[ind-1][start]
+
+                    dp[ind][start] = max(end_cur_sub_arr, end_at_other)
+        
+        return dp[n][0]
+                
