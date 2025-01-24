17af9a471959dbd2a1bbe8423e2dfb908c50dac0
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-01-24 22:01:55 +0530

    Time: 146 ms (9.76%) | Memory: 27 MB (22.87%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,38 +1,16 @@
-# tabulation approach
 class Solution:
-    def maxProfit(self, prices: list[int]) -> int:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        best_buy_price = float('inf')
+
+        max_profit = 0
 
         n = len(prices)
+
+        for cur_day in range(n):
+
+            best_buy_price = min(prices[cur_day], best_buy_price)
+
+            max_profit = max(max_profit, prices[cur_day] - best_buy_price)
         
-        dp = [-float('inf') for buy in range(2)]
-
-        dp[0] = dp[1] = 0
-
-        cur_dp = [-float('inf') for i in range(2)]
-
-        for day in reversed(range(n)):
-
-            for buy in range(2):
-
-                if buy == 0:
-
-                    buy_profit = -prices[day] + dp[1]
-
-                    buy_on_other = dp[buy]  
-
-                    cur_dp[buy] = max(buy_profit, buy_on_other)
-                
-                elif buy == 1:
-
-                    sell_profit = prices[day] 
-
-                    sell_on_other = dp[buy]
-
-                    cur_dp[buy] = max(sell_profit, sell_on_other)
-            
-            dp = cur_dp
-
-        return dp[0]
-
-
-
+        return max_profit
