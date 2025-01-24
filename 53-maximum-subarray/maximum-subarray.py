096e49210bb694790cfb591da5934d92668dbae7
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-01-24 21:04:48 +0530

    Time: 1280 ms (5.03%) | Memory: 74.9 MB (5.58%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,20 +1,45 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
-        cur_sub_arr_sum = nums[0]
+        # nums = [-2,1,-3,4,-1,2,1,-5,4]
+        # o/p : 6
 
-        mx_sub_arr_sum = nums[0]
+        def getMaxSubarrSum(n,start):
+
+          if n == 1:
+
+            if start == 0:
+
+                return nums[0]
+            
+            else:
+                
+                return max(0,nums[0])
+
+          if dp[n][start] != -float('inf'):
+
+            return dp[n][start]
+
+          if start == 0:
+
+            start_new_sub_arr = nums[n-1] + getMaxSubarrSum(n-1,1)
+
+            start_at_other = getMaxSubarrSum(n-1,start)
+
+            dp[n][start] = max(start_new_sub_arr, start_at_other)
+          
+          elif start == 1:
+
+            end_cur_sub_arr = 0
+            
+            end_at_other = nums[n-1] + getMaxSubarrSum(n-1,start)
+
+            dp[n][start] = max(end_cur_sub_arr, end_at_other)
+          
+          return dp[n][start]
 
         n = len(nums)
 
-        for i in range(1,n):
+        dp = [[-float('inf') for start in range(2)] for ind in range(n+1)]
 
-            if cur_sub_arr_sum < 0 :
-
-                cur_sub_arr_sum = 0
-            
-            cur_sub_arr_sum += nums[i]
-
-            mx_sub_arr_sum = max(mx_sub_arr_sum, cur_sub_arr_sum)
-        
-        return mx_sub_arr_sum
+        return getMaxSubarrSum(n,0)
