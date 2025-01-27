dd030d956f1705003c6e95498855651cac401f04
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-01-27 23:28:53 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (50.62%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -5,30 +5,15 @@ class Solution:
 
         n = len(matrix[0])
 
-        low = 0
+        for i in range(m):
 
-        high = m*n - 1
+            for j in range(n):
 
-        while low <= high:
+                if matrix[i][j] == target:
 
-            mid = (low + high)//2
-
-            mid_row, mid_col = divmod(mid,n)
-
-            mid_ele = matrix[mid_row][mid_col]
-
-            if  mid_ele == target:
-
-                return True
-            
-            elif target < mid_ele:
-
-                high = mid - 1
-            
-            elif target > mid_ele:
-
-                low = mid + 1
+                    return True
         
         return False
+
 
                 
