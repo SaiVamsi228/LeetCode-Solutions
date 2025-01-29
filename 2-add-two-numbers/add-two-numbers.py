0640584f35b7f61ace5b24763d2cd75683861015
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-01-29 22:04:02 +0530

    Time: 3 ms (73.22%) | Memory: 17.9 MB (41.66%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -4,58 +4,73 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        dummy_node = ListNode(-1,None)
 
-        temp1 = l1
-
-        temp2 = l2
+        temp = dummy_node
 
         carry = 0
 
-        dummyNode = ListNode(-1)
+        while l1 and l2:
 
-        temp = dummyNode
+            sm = l1.val + l2.val + carry
 
-        while temp1 or temp2 or carry :
+            carry = sm//10
 
-            newNodeVal = 0
-            
-            if temp1:
-                
-                newNodeVal += temp1.val 
+            sm = sm % 10
 
-                temp1 = temp1.next
-            
-            if temp2:
-                
-                newNodeVal += temp2.val 
-                
-                temp2 = temp2.next
+            new_node = ListNode(sm,None)
 
-
-            if carry :
-
-                newNodeVal += carry
-
-                carry = 0
-
-            if newNodeVal >=10:
-                
-                carry = newNodeVal // 10
-
-                newNodeVal = newNodeVal % 10
-
-            temp.next = ListNode(newNodeVal)
+            temp.next = new_node
 
             temp = temp.next
 
-        if carry :
+            l1 = l1.next
 
-            temp.next = ListNode(carry)
+            l2 = l2.next
 
         
-        newHead = dummyNode.next
+        while l1 :
 
-        return newHead
+            sm = l1.val + carry
+
+            carry = sm//10
+
+            sm = sm % 10
+
+            new_node = ListNode(sm,None)
+
+            temp.next = new_node
+
+            temp = temp.next 
+
+            l1 = l1.next
         
+        while l2:
+
+            sm = l2.val + carry
+
+            carry = sm//10
+
+            sm = sm % 10
+
+            new_node = ListNode(sm,None)
+
+            temp.next = new_node
+
+            temp = temp.next 
+
+            l2 = l2.next
+        
+        while carry:
+
+            carry_node = ListNode(carry%10,None)
+
+            temp.next = carry_node
+
+            carry //= 10
+        
+        return dummy_node.next
+
+
