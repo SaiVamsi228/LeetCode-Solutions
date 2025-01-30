cfd486cdda0c4f386d3f0839745c1ffc2b02eeaa
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-01-30 21:25:10 +0530

    Time: 32 ms (61.45%) | Memory: 39.4 MB (31.91%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,59 +7,25 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         
-        def reverse_list(head):
-
-            prev = None
-
-            curr = head
-
-            upcoming = head.next
-
-            while curr:
-
-                curr.next = prev
-
-                prev = curr
-
-                curr = upcoming
-
-                if curr != None:
-
-                    upcoming = curr.next
-            
-            return prev
-
-        if not head or not head.next:
-
-            return True
-
-        slow = head
-
-        fast = head
-
-
-        while fast and fast.next:
-
-            slow = slow.next
-
-            fast = fast.next.next
+        st = deque()
         
-        new_head = reverse_list(slow)
+        temp = head
+        
+        while temp is not None:
+            
+            st.append(temp.val)
+            
+            temp = temp.next
+            
+        temp = head
+        
+        while temp is not None:
 
-
-        temp1, temp2 = head, new_head
-
-        while temp1 and temp2 :
-
-            if temp1.val != temp2.val:
-
+            if temp.val != st.pop():
+                
                 return False
-            
-            temp1 = temp1.next
+                
+            temp = temp.next
 
-            temp2 = temp2.next
-
-        
         return True
-
 
