cbe51f35699047f6110b5075427d776e674a6162
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-01-30 14:12:55 +0530

    Time: 13 ms (97.17%) | Memory: 39.3 MB (31.91%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -3,68 +3,18 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        if not head or not head.next:
-
-            return head
-
-        prev = None
-
-        cur = head
-
-        upcoming = head.next
-
-        while cur:
-
-            upcoming  = cur.next
-
-            cur.next = prev
-
-            prev = cur
-
-            cur = upcoming
-        
-        return prev
-
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         
-        slow = head
+        arr = []
 
-        fast = head
+        temp = head
 
-        while fast and fast.next:
+        while temp:
 
-            slow = slow.next 
+            arr.append(temp.val)
 
-            fast = fast.next.next
-
-        newHead = self.reverseList(slow)
-
-        firstHalfPt = head
-
-        secondHalfPt = newHead
-
-        while firstHalfPt and secondHalfPt:
-
-            if firstHalfPt.val != secondHalfPt.val:
-
-                self.reverseList(newHead)
-
-                return False
-
-            firstHalfPt = firstHalfPt.next
-
-            secondHalfPt = secondHalfPt.next
+            temp = temp.next
         
-        self.reverseList(newHead)
+        return arr == arr[::-1]
 
-        return True
-        
-        
