99c13108166f6c3b74d6f81e87cbd5a91212864a
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-02-10 10:12:09 +0530

    Time: 2 ms (40.81%) | Memory: 18 MB (51.47%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,31 +1,12 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-        def findPermutations(arr, start, ans):
-    
-            if start == len(arr) - 1:
-                
-                ans.append(arr.copy())
-                
-                return
-            
-            mp = set()
-            
-            for i in range(start, len(arr)):
-                if arr[i] not in mp:
-                    
-                    arr[start], arr[i] =  arr[i], arr[start]
-                    
-                    findPermutations(arr, start+1, ans)
-                    
-                    arr[start], arr[i] =  arr[i], arr[start]
-                
-                    mp.add(arr[i])
-            
-            return
+        all_perm = permutations(nums)
 
         ans = []
 
-        findPermutations(nums, 0 , ans)
+        for perm in all_perm:
 
+            ans.append(list(perm))
+        
         return ans
