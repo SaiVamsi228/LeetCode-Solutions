5e357145df850e7801ef7ae5b411fa51fee390ee
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-02-14 15:06:52 +0530

    Time: 0 ms (100.00%) | Memory: 24.8 MB (80.09%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -3,10 +3,43 @@ class Solution:
         
         n = len(nums)
 
-        for i in range(0,n,2):
+        low , high = 0, n - 1
 
-            if i + 1 < n and nums[i] != nums[i+1]:
+        if n == 1 :
 
-                return nums[i]
+            return nums[0]
+
+
+        while low <= high :
+
+            mid = (low + high)//2
+
+            if mid == n - 1:
+
+                return nums[mid]
+
+            if mid % 2 == 0 :
+
+                if nums[mid] == nums[mid + 1] :
+
+                    low = mid + 1
+                
+                else:
+
+                    high = mid - 1
+            
+            else:
+
+                if nums[mid] != nums[mid+1]:
+
+                    low = mid + 1
+                
+                else:
+
+                    high = mid - 1
         
-        return nums[-1]
+        if low == n :
+
+            return nums[n-1]
+            
+        return nums[low] 
