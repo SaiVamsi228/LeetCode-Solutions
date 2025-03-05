bb98c2653a7e52f7ec7b20ee4346f8988349b80e
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-03-05 11:13:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.62%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,24 +1,35 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
-        def get_path_cnt(row,col):
-
-            if row == m-1 and col == n-1 :
-
-                return 1
-            
-            if row >= m or col >= n :
-
-                return 0
-            
-            if dp[row][col] != -1:
-
-                return dp[row][col]
-            
-            dp[row][col] = get_path_cnt(row+1,col) + get_path_cnt(row,col+1)
-
-            return dp[row][col]
 
         dp = [ [-1 for i in range(n)] for j in range(m)]
 
-        return get_path_cnt(0,0)
+        dp[m-1][n-1] = 1
+
+        for row in reversed(range(m)):
+
+            for col in reversed(range(n)):
+
+                if row == m-1 and col == n-1 :
+
+                    continue
+                
+                if row + 1 < m :
+
+                    bottom_ways = dp[row+1][col]
+                
+                else:
+
+                    bottom_ways = 0
+                
+                if col + 1 < n:
+
+                    right_ways = dp[row][col+1]
+                
+                else:
+
+                    right_ways = 0
+                
+                dp[row][col] = bottom_ways + right_ways
+
+
+        return dp[0][0]
