bf331e82dd89ab9c2673682ee69b3ebe1da0cd3c
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-03-10 15:42:31 +0530

    Time: 65 ms (12.23%) | Memory: 30.3 MB (96.21%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,4 +1,3 @@
-from collections import deque
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -7,25 +6,80 @@ from collections import deque
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         
-        st = deque()
-        
-        temp = head
-        
-        while temp is not None:
-            
-            st.append(temp.val)
-            
-            temp = temp.next
-            
-        temp = head
-        
-        while temp is not None:
+        def reverse_ll(head,k):
 
-            if temp.val != st.pop():
-                
+            prev = None
+
+            curr = head
+
+            while curr and k > 0:
+
+                upcoming = curr.next
+
+                curr.next = prev
+
+                k -= 1
+
+                prev = curr
+
+                curr = upcoming
+            
+            new_head = prev
+
+            return new_head
+        
+        slow = head
+
+        fast = head
+
+        prev = None
+
+        k = 1
+
+        while fast and fast.next:
+
+            prev = slow
+
+            slow = slow.next 
+
+            k += 1
+
+            fast = fast.next.next
+
+        if fast:
+
+            # it is odd length
+
+            temp2 = slow.next
+
+            temp1 = reverse_ll(head,k)
+
+            temp1 = temp1.next
+        
+        else:
+
+            # it is of even length
+
+            # so slow is one step ahead lets use prev
+
+            temp2 = slow
+
+            temp1 = reverse_ll(head,k - 1)
+        
+        while temp1 and temp2:
+
+            if temp1.val != temp2.val:
+
                 return False
-                
-            temp = temp.next
+            
+            temp1 = temp1.next
 
+            temp2 = temp2.next
+        
         return True
+
+
+            
+
+
 
