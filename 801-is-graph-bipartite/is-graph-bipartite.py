79f1c096ae6b4790cfc615bfdb77d7f885c61410
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-05-15 09:51:57 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (36.26%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,40 +1,57 @@
 class Solution:
-    def getIsBipartite(self,v, graph, visited,actColor):
-
-        if v in visited:
-
-            if visited[v] != actColor:
-                            
-                return False
-            
-            return True
-        
-        visited[v] = actColor
-                
-        isBipartite = True
-
-        for neighbour in graph[v]:
-
-            isBipartite = isBipartite and self.getIsBipartite(neighbour, graph, visited,actColor ^ 1)
-
-        return isBipartite
-        
-                        
     def isBipartite(self, graph: List[List[int]]) -> bool:
         
-        visited = {}
-        
-        V = len(graph)
-        
-        for v in range(V):
-            
-            if v not in visited :
-                
-                if not self.getIsBipartite(v, graph, visited,0):
+        n = len(graph)
+
+        visited_color = [0 for i in range(n)]
+
+        def getIsBipartite(node):
+
+            q = deque()
+
+            parent = -1
+
+            q.append((node,parent))
+
+            while q :
+
+                node,par = q.popleft()
+
+                color = visited_color[node]
+
+                for neighbour in graph[node]:
+
+                    if neighbour == par:
+
+                        continue
                     
+                    if visited_color[neighbour]:
+
+                        n_color = visited_color[neighbour]
+
+                        if color == n_color:
+
+                            return False
+                    
+                    else:
+
+                        n_color = 1 if color == 2 else 2
+
+                        visited_color[neighbour] = n_color
+
+                        q.append((neighbour,node))
+            
+            return True
+
+
+        
+
+        for node in range(n):
+
+            if not visited_color[node]:
+
+                if not getIsBipartite(node):
+
                     return False
         
         return True
-        
-        
-        
