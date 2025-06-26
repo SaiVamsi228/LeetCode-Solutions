25e8481b1a34ccc7f3a1d64fa2e319a18233d2a4
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-06-26 14:51:25 +0530

    Time: 409 ms (26.06%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,7 +1,5 @@
-SELECT DISTINCT P1.email
-FROM Person P1
-JOIN
-Person P2
-ON P1.id != P2.id and P1.email = P2.email;
-
-
+# Write your MySQL query statement below
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
