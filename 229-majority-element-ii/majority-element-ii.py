6c765402bec65de4d89b53c4b7f72305a79f2b78
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-07-16 22:34:44 +0530

    Time: 11 ms (30.94%) | Memory: 20.6 MB (64.39%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,64 +1,68 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         
-        majority_1 = majority_2 = -1
+        f1,f2 = 0, 0
 
-        cnt_1 = cnt_2 = 0
+        e1, e2 = None, None
 
-        for num in nums:
+        n = len(nums)
 
-            if num == majority_1:
+        for i in range(n):
 
-                cnt_1 += 1
+            if f1 == 0 and nums[i] != e2:
+
+                f1 = 1
+
+                e1 = nums[i]
             
-            elif num == majority_2 :
+            elif f2 == 0 and nums[i] != e1:
 
-                cnt_2 += 1
+                f2 = 1
+
+                e2 = nums[i]
+
+            elif nums[i] == e1 :
+
+                f1 += 1
             
-            elif cnt_1 == 0 :
+            elif nums[i] == e2:
 
-                majority_1 = num
-
-                cnt_1 = 1
-            
-            elif cnt_2 == 0:
-
-                majority_2 = num
-
-                cnt_2 = 1
+                f2 += 1
             
             else:
 
-                cnt_1 -= 1
+                f1 -= 1
 
-                cnt_2 -= 1
-
-        act_cnt_1 = act_cnt_2 = 0
-
-        for num in nums:
-
-            if num == majority_1 :
-
-                act_cnt_1 += 1
-
-            elif num == majority_2:
-
-                act_cnt_2 += 1
-                
-        n = len(nums)
+                f2 -= 1
         
-        if act_cnt_1 > n//3 and act_cnt_2 > n//3:
+        cnt1, cnt2 = 0, 0
 
-            return [majority_1,majority_2]
+        for i in range(n):
+
+            if nums[i] == e1 :
+
+                cnt1 += 1
+            
+            if nums[i] == e2:
+
+                cnt2 += 1
         
-        elif act_cnt_1 > n//3 :
+        if cnt1 > n//3 and cnt2 > n//3:
 
-            return [majority_1]
+            return [e1,e2]
         
-        elif act_cnt_2 > n//3:
+        elif cnt1 > n//3:
 
-            return [majority_2]
+            return [e1]
         
-        else:
+        elif cnt2 > n//3:
+            
+            return [e2]
+        
+        return []
 
-            return []
+
+
+
+            
+            
