ff2420cabf1a037678b674b4b9412ec402efea7e
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-07-16 18:49:55 +0530

    Time: 42 ms (23.70%) | Memory: 20.4 MB (71.67%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,30 +1,28 @@
 from collections import defaultdict
-
-
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        
+        hm = defaultdict(int)
+
+        hm[0] = 1
+
+        sm = 0
 
         n = len(nums)
 
-        h_mp = defaultdict(int)
-
-        pre_sum = 0
-
         cnt = 0
-
-        h_mp[0] = 1  # MOST IMP: IF K ITSELF EXISTS THEN K-K =0 WHOSE FREQ SHOULD BE 1
 
         for i in range(n):
 
-            pre_sum += nums[i]
+            sm += nums[i]
+            
+            req = sm - k
 
-            remove = (pre_sum - k)  
-            # IMP : WE ARE VERIFYING IF THERE FORMS A SUBARRAY UPTO INDEX 
-            #       I BY SEARCHING FOR X-K IF IT EXISTS THEN IT IS SURE THAT K IS POSSIBLE
-            #       IF NUMBER OF X-K SUBARRAYS EXIST THEN WE CAN FORM THAT MANY K SUM SUBARRAYS
+            if req in hm:
 
-            cnt += h_mp[remove]
+                cnt += hm[req]
 
-            h_mp[pre_sum] += 1
-
+            hm[sm] += 1
+        
         return cnt
+            
