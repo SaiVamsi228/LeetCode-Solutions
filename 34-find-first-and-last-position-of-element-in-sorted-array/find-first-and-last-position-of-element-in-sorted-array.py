87f6b30261d0458245eda79db377332149c8b5a4
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-07-18 12:02:33 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (75.64%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,76 +1,37 @@
 class Solution:
-
-    def getFirstOccurence(self,nums,target):
-
-        n = len(nums)
-
-        low, high = 0, n-1
-
-        left = -1
-
-        while low <= high:
-
-            mid = (low+high)//2
-
-            
-            if nums[mid] >= target:
-
-                if nums[mid] == target:
-
-                    if left == -1:
-
-                        left = mid
-                    
-                    if mid < left :
-
-                        left = mid
-                
-                high = mid - 1
-                
-            else:
-
-                low = mid + 1
-        
-        return left
-
-    def getLastOccurence(self,nums,target):
-
-        n = len(nums)
-
-        low, high = 0, n-1
-
-        right = -1
-
-        while low <= high:
-
-            mid = (low+high)//2
-
-            if nums[mid] <= target:
-
-                if nums[mid] == target:
-
-                    if right == -1:
-
-                        right = mid
-                    
-                    if mid > right :
-
-                        right = mid
-
-                    
-                low = mid + 1
-                
-            else:
-
-                high = mid - 1
-        
-        return right
-
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
-
-        first = self.getFirstOccurence(nums,target)
-
-        last = self.getLastOccurence(nums,target)
-
-        return [first,last]
+        def getFO(arr):
+            n = len(arr)
+            left = 0
+            right = n - 1
+            ans = -1
+            while left <= right:
+                mid = (left + right)//2
+                if arr[mid] == target:
+                    ans = mid
+                    right = mid - 1
+                elif arr[mid] > target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            return ans
+        
+        def getLO(arr):
+            n = len(arr)
+            left = 0
+            right = n - 1
+            ans = -1
+            while left <= right:
+                mid = (left + right)//2
+                if arr[mid] == target:
+                    ans = mid
+                    left = mid + 1
+                elif arr[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return ans
+        
+        return [getFO(nums),getLO(nums)]
+            
