76d5a04c596dcbfb0f9c81a2f706d3c77473995b
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-07-19 23:30:17 +0530

    Time: 5 ms (65.02%) | Memory: 18 MB (36.70%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,35 +1,38 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-
-        if len(s) != len(t):
-
-            return False
         
         h1 = {}
 
         h2 = {}
 
+        if len(s) != len(t):
+
+            return False
+        
         n = len(s)
 
-        for i in range(n):
+        for i,char in enumerate(s):
 
-            if s[i] not in h1 and t[i] not in h2:
+            if char in h1:
 
-                h1[s[i]] = t[i]
-
-                h2[t[i]] = s[i]
-            
-            elif s[i] in h1 and t[i] in h2 :
-
-                if t[i] != h1[s[i]] or s[i] != h2[t[i]]:
+                if h1[char] != t[i]:
 
                     return False
+            
+            else:
 
-            elif s[i] in h1 or t[i] in h2:
-
-                return False
+                h1[char] = t[i]
         
+        for i,char in enumerate(t):
+
+            if char in h2:
+
+                if h2[char] != s[i]:
+
+                    return False
+            
+            else:
+
+                h2[char] = s[i]
+    
         return True
-
-
-        
