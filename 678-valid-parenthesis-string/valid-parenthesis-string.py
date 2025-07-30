f82b06b663ded3f8369f1a6961cb33b3c79fc9b5
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-07-30 23:18:45 +0530

    Time: 29 ms (5.94%) | Memory: 17.9 MB (39.90%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,48 +1,55 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         
-        def isPossible(i,balance):
-
-            if i == n :
-
-                if balance == 0:
-
-                    return True
-                
-                return False
-            
-            if balance < 0 :
-
-                return False
-            
-            if (i,balance) in dp:
-                
-                return dp[(i,balance)]
-
-            char = s[i]
-
-            if char == "(":
-
-                dp[(i,balance)] = isPossible(i+1,balance + 1)
-
-                return dp[(i,balance)]
-            
-            elif char == ")":
-
-                dp[(i,balance)] = isPossible(i+1,balance-1)
-
-                return dp[(i,balance)]
-            
-            elif char == "*":
-
-                dp[(i,balance)] = isPossible(i+1,balance) or isPossible(i+1,balance+1) or isPossible(i+1,balance - 1)
-
-                return dp[(i,balance)] 
-            
-        
         n = len(s)
 
-        dp = {}
+        dp = [[False for bal in range(n+1)] for i in range(n+1)]
+
+        balance = 0
+
+        dp[n][balance] = True 
+
+        for i in reversed(range(n)):
+
+            for balance in range(n):
+
+                if i == n and balance == 0:
+
+                    continue
+
+                if s[i] == "(":
+
+                    if balance + 1 <= n :
+
+                        dp[i][balance] = dp[i+1][balance + 1]
+                
+                elif s[i] == ")":
+
+                    if balance - 1 >= 0 :
+
+                        dp[i][balance] = dp[i+1][balance - 1]
+
+                elif s[i] == "*":
+
+                    bal_plus_one = dp[i+1][balance + 1] 
+                    
+                    bal_minus_one = dp[i+1][balance - 1] 
+
+                    only_bal = dp[i+1][balance] 
+                    
+                    dp[i][balance] =  only_bal or bal_plus_one or bal_minus_one
+        
+
+        return dp[0][0]
+                    
+
+                
+
+                
+
+                
+
+
 
         return isPossible(0,0)
             
