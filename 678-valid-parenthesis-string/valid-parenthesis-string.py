0feaac6e49ca6f1cf088227e22718974bca014ff
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-07-31 20:27:05 +0530

    Time: 28 ms (6.06%) | Memory: 17.9 MB (40.04%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,23 +1,27 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         n = len(s)
-        dp = [[False for _ in range(n + 1)] for _ in range(n + 1)]
+        dp = [False for _ in range(n + 1)]
 
-        dp[n][0] = True  # base case
+        dp[0] = True  # base case
 
         for i in range(n - 1, -1, -1):
+            cur_dp = [False for i in range(n+1)]
             for bal in range(n):
                 if s[i] == '(':
                     if bal + 1 <= n:
-                        dp[i][bal] = dp[i + 1][bal + 1]
+                        cur_dp[bal] = dp[bal + 1]
                 elif s[i] == ')':
                     if bal - 1 >= 0:
-                        dp[i][bal] = dp[i + 1][bal - 1]
+                        cur_dp[bal] = dp[bal - 1]
                 elif s[i] == '*':
-                    dp[i][bal] = dp[i + 1][bal]
+                    cur_dp[bal] = dp[bal]
                     if bal + 1 <= n:
-                        dp[i][bal] |= dp[i + 1][bal + 1]
+                        cur_dp[bal] |= dp[bal + 1]
                     if bal - 1 >= 0:
-                        dp[i][bal] |= dp[i + 1][bal - 1]
+                        cur_dp[bal] |= dp[bal - 1]
+            
+            dp = cur_dp.copy()
 
-        return dp[0][0]
+
+        return dp[0]
