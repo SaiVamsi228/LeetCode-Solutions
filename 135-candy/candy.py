083b7774676baff82f4b060fa2b4741045bfafc5
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-08-04 10:01:17 +0530

    Time: 15 ms (71.87%) | Memory: 19.8 MB (91.18%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,68 +1,16 @@
-from heapq import heapify, heappush, heappop
-class Solution:
-    def candy(self, ratings: List[int]) -> int:
-        
+class Solution(object):
+    def candy(self, ratings):
         n = len(ratings)
-
-        if n == 1:
-
-            return 1
-            
-        candies = [1 for i in range(n)]
-
-        min_hp = [] # (rating,ind)
-
-        heapify(min_hp)
-
-        for ind in range(n):
-            
-            rating = ratings[ind]
-
-            heappush(min_hp,(rating,ind))
-
-        min_candies_req = n
-
-        while min_hp:
-
-            rating,ind = heappop(min_hp)
-
-            if ind == 0 :
-
-                if ratings[0] > ratings[1]:
-
-                    candies[ind] = candies[1] + 1 
-            
-            elif ind == n - 1:
-
-                if ratings[n-1] > ratings[n-2]:
-
-                    candies[ind] = candies[n-2] + 1
-            
-            else:
-
-                prev, after = candies[ind-1], candies[ind+1]
-
-                mini = 0
-
-                if ratings[ind] == ratings[ind-1] and ratings[ind] == ratings[ind+1]:
-
-                    candies[ind] = after
-                
-                if ratings[ind] > ratings[ind+1]:
-
-                    mini = candies[ind+1] + 1
-
-                    candies[ind] = mini
-                
-                if ratings[ind] > ratings[ind-1]:
-
-                    mini = max(candies[ind-1]+1,mini)
-                
-                    candies[ind] = mini
-                
-            min_candies_req += candies[ind] - 1
+        cnt = 0
+        candies = [1] * n
         
-        return min_candies_req
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
 
+        for i in reversed(range(n-1)):
+            if ratings[i] > ratings[i+1]:
+                candies[i] = max(candies[i+1] + 1, candies[i])
+            cnt += candies[i]
 
-                
+        return cnt + candies[n - 1]
