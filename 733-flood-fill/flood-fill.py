e69c8dac6988b656700b45639b4b6b4f24311dc1
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-08-06 15:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (69.83%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,47 +1,52 @@
+from collections import deque
 class Solution:
-    def isValid(self,row,col,m,n):
-
-        if 0 <= row < m and 0 <= col < n:
-
-            return True
-        
-        return False
-    
-    def dfs(self,row,col,m,n,image,color,srcCol):
-
-        if not self.isValid(row,col,m,n) :
-
-            return
-        
-        if image[row][col] == color:
-
-            return 
-        
-        if image[row][col] != srcCol : 
-            
-            return 
-        
-        image[row][col] = color
-
-        drow = [1,-1,0,0]
-        dcol = [0,0,1,-1]
-
-        for i in range(4):
-
-            nRow , nCol = row + drow[i] , col + dcol[i]
-
-            self.dfs(nRow,nCol,m,n,image,color,srcCol)
-        
-        return 
-
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         
-        row,col = sr,sc
+        q = deque()
 
-        m,n = len(image), len(image[0])
+        visited = set()
 
-        srcCol = image[sr][sc]
+        q.append((sr,sc))
 
-        self.dfs(row,col,m,n,image,color,srcCol)
+        visited.add((sr,sc))
 
+        m = len(image)
+
+        n = len(image[0])
+
+        initial_color = image[sr][sc]
+
+        image[sr][sc] = color
+
+        dr = [0,0,1,-1]
+
+        dc = [1,-1,0,0]
+
+        def isValid(row, col, m , n ):
+
+            return 0 <= row < m and 0 <= col < n 
+
+        while q:
+
+            r,c = q.popleft()
+
+            for i in range(4):
+
+                new_row, new_col = r + dr[i], c + dc[i]
+
+                if isValid(new_row,new_col,m,n):
+
+                    if (new_row,new_col) not in visited:
+
+                        visited.add((new_row,new_col))
+
+                        if image[new_row][new_col] == initial_color:
+                            
+                            image[new_row][new_col] = color
+
+                            q.append((new_row,new_col))
+        
         return image
+
+
+                    
