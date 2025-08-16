153e75e25735be225fd5838ab33533375ac959bd
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-08-16 13:03:20 +0530

    Time: 3 ms (10.60%) | Memory: 18 MB (17.53%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,34 +1,31 @@
+from functools import lru_cache
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def getMaxRobbery(nums):
+        
+        @lru_cache(None)
+        def getMaxMoney(ind,first_house_robbed):
 
-            n = len(nums)
+            if ind >= n :
 
-            dp = [0 for i in range(n+1)]
+                return 0 
+            
+            if ind == n - 1 and first_house_robbed:
 
-            dp[1] = nums[0]
+                return 0
+            
+            if ind == 0:
 
-            if n == 1:
+                rob_money = nums[ind] + getMaxMoney(ind+2,1)
+            
+            else:
+                
+                rob_money = nums[ind] + getMaxMoney(ind+2,first_house_robbed)
+            
+            rob_others = getMaxMoney(ind+1,first_house_robbed)
 
-                return dp[1]
-
-            dp[2] = max(nums[0], nums[1])
-
-            if n == 2:
-
-                return dp[2]
-
-            for i in range(3,n+1):
-
-                dp[i] = max(nums[i-1] + dp[i-2], dp[i-1])
-
-            return dp[n]
+            return max(rob_money,rob_others)
         
         n = len(nums)
 
-        if n == 1:
+        return getMaxMoney(0,0)
 
-            return nums[0]
-        # because we cant rob simulataneously
-        
-        return max(getMaxRobbery(nums[:-1]), getMaxRobbery(nums[1:]))
