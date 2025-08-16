074bc25100d95ff59e5f65ba002396642046a104
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-08-16 12:18:32 +0530

    Time: 3 ms (7.25%) | Memory: 18 MB (12.78%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,24 +1,26 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        
+        def getMaxMoney(ind):
 
+            if ind >= n :
+
+                return 0
+            
+            if ind in dp:
+
+                return dp[ind]
+
+            rob_money = nums[ind] + getMaxMoney(ind+2)
+
+            rob_others = getMaxMoney(ind+1)
+
+            dp[ind] = max(rob_money,rob_others)
+
+            return dp[ind]
+        
         n = len(nums)
 
-        dp = [0 for i in range(n+1)]
-
-        dp[1] = nums[0]
-
-        if n == 1:
-
-            return dp[1]
-
-        dp[2] = max(nums[0], nums[1])
-
-        if n == 2:
-
-            return dp[2]
-
-        for i in range(3,n+1):
-
-            dp[i] = max(nums[i-1] + dp[i-2], dp[i-1])
-
-        return dp[n]
+        dp = {}
+        
+        return getMaxMoney(0)
