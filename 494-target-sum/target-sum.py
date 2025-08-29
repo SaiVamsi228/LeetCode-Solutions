c310ab56b541d95ed562a2e38fb730b2c5f02deb
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-08-29 10:16:19 +0530

    Time: 200 ms (27.03%) | Memory: 35.1 MB (37.95%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,40 +1,31 @@
+from functools import lru_cache
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
+        def getCntSubsetSum(n,target):
+
+            if n == 0:
+
+                if target == 0:
+
+                    return 1
+
+                return 0
+            
+            if (n,target) in dp:
+
+                return dp[(n,target)]
+            
+            add_cnt = getCntSubsetSum(n-1,target-nums[n-1])
+
+            sub_cnt = getCntSubsetSum(n-1,target+nums[n-1])
+
+            dp[(n,target)] =  add_cnt + sub_cnt
+
+            return dp[(n,target)]
+        
         n = len(nums)
 
-        totSum = sum(nums)
+        dp = {}
 
-        dp = [ [0 for i in range(totSum + 1)] for j in range(n+1)]
-
-        dp[0][0] = 1
-
-        # s1 + s2 = totSum
-        # s1 - s2 = target
-        newTotSum = totSum - target
-
-        if newTotSum % 2 == 1 or abs(target) > totSum :
-
-            return 0
-
-        for i in range(1,n+1):
-
-            for j in range(totSum + 1):
-
-                if nums[i-1] <= j:
-
-                    take = dp[i-1][j - nums[i-1]]
-
-                    notTake = dp[i-1][j]
-
-                    dp[i][j] = take + notTake
-                
-                else:
-
-                    dp[i][j] = dp[i-1][j]
-        
-        
-        
-        searchSum = newTotSum // 2
-
-        return dp[n][searchSum]
+        return getCntSubsetSum(n,target) 
