78856bae54dca719d55c1064b93a812ba0cf9d63
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-08-29 16:20:00 +0530

    Time: 139 ms (54.11%) | Memory: 28.7 MB (39.65%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,4 +1,3 @@
-from functools import lru_cache
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
@@ -16,16 +15,28 @@ class Solution:
 
                 return dp[(n,target)]
             
-            add_cnt = getCntSubsetSum(n-1,target-nums[n-1])
+            take_cnt = getCntSubsetSum(n-1,target-nums[n-1])
 
-            sub_cnt = getCntSubsetSum(n-1,target+nums[n-1])
+            not_take_cnt = getCntSubsetSum(n-1,target)
 
-            dp[(n,target)] =  add_cnt + sub_cnt
+            dp[(n,target)] =  take_cnt + not_take_cnt
 
             return dp[(n,target)]
         
         n = len(nums)
 
+        tot_sum = sum(nums)
+
+        if (tot_sum + target)%2 != 0 :
+
+            return 0
+        
+        if target > tot_sum :
+
+            return 0
+
+        target_sum = (tot_sum + target)//2
+
         dp = {}
 
-        return getCntSubsetSum(n,target) 
+        return getCntSubsetSum(n,target_sum) 
