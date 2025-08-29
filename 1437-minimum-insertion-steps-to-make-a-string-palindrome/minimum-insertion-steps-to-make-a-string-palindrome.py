a0d74436f6a29e6f3412bbc579f24d2d06460a35
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-08-29 23:15:00 +0530

    Time: 1854 ms (5.02%) | Memory: 360.3 MB (5.02%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,25 +1,35 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        def getLCS(s1,s2):
-
-            m = len(s1)
-
-            n = len(s2)
-
-            dp = [ [0 for i in range(n+1)] for j in range(m+1)] #base case initialized
-
-            for i in range(1,m+1):
-
-                for j in range(1,n+1):
-
-                    if s1[i-1] == s2[j-1]:
-
-                        dp[i][j] = 1 + dp[i-1][j-1]
-                    
-                    else:
-
-                        dp[i][j] = max(dp[i][j-1], dp[i-1][j])
-            
-            return dp[m][n]
         
-        return len(s) - getLCS(s,s[::-1]) 
+
+        def get_LCS(m,n):
+
+            if m == 0 or n == 0:
+
+                return 0
+            
+            if (m,n) in dp:
+
+                return dp[(m,n)]
+            
+            if s1[m-1] == s2[n-1]:
+
+                dp[(m,n)] = 1 + get_LCS(m-1,n-1)
+            
+            else:
+
+                dp[(m,n)] = max(get_LCS(m-1,n), get_LCS(m,n-1))
+            
+            return dp[(m,n)]
+            
+        s1 = s
+
+        s2 = s[::-1]
+
+        dp = {}
+
+        m = len(s1)
+
+        n = len(s2)
+
+        return m - get_LCS(m,n)
