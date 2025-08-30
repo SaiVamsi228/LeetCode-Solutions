07c0bbd9dd0e5cf42d08e7aeeab4dc30cfc5610b
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-08-30 10:47:01 +0530

    Time: 561 ms (26.22%) | Memory: 43.8 MB (27.81%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.py
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.py
@@ -1,24 +1,30 @@
 class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
         
-        def getLCSDP(s1,s2,m,n):
+        def lcs(m,n):
 
-            dp = [ [0 for i in range(n+1)] for j in range(m+1)] #base case initialized
+            if m == 0 or n == 0:
 
-            for i in range(1,m+1):
-
-                for j in range(1,n+1):
-
-                    if s1[i-1] == s2[j-1]:
-
-                        dp[i][j] = 1 + dp[i-1][j-1]
-                    
-                    else:
-
-                        dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+                return 0
             
-            return dp
-        
+            if dp[m][n] != -1:
+
+                return dp[m][n]
+            
+            if s1[m-1] == s2[n-1]:
+
+                dp[m][n] =  1 + lcs(m-1,n-1) 
+            
+            else:
+
+                lcs_1 = lcs(m,n-1)
+
+                lcs_2 = lcs(m-1,n)
+
+                dp[m][n] = max(lcs_1,lcs_2)
+            
+            return dp[m][n]
+
         s1 = str1
 
         s2 = str2
@@ -27,48 +33,57 @@ class Solution:
 
         n = len(s2)
 
-        dp = getLCSDP(s1,s2,m,n)
+        dp = [ [-1 for i in range(n+1)] for j in range(m+1)]
 
-        superSeq = []
+        lcs(m,n)
 
-        i = m 
+        ans = []
 
-        j = n 
+        i , j = m - 1 , n - 1
 
-        while i > 0 and j > 0 :
-            
-            if s1[i-1] == s2[j-1]:
+        while i >= 0 and j >= 0:
 
-                superSeq.append(s1[i-1])
+            if s1[i] == s2[j]:
+
+                ans.append(s1[i])
 
                 i -= 1
 
                 j -= 1
             
-            elif dp[i-1][j] >= dp[i][j-1]:
-
-                superSeq.append(s1[i - 1])
-
-                i -= 1
-
             else:
 
-                superSeq.append(s2[j - 1])
+                upper_val = dp[i-1 +1][j +1]
 
-                j -= 1
+                left_val = dp[i +1][j-1 +1]
+
+                cur_val = dp[i +1][j +1]
+
+                if cur_val == upper_val :
+
+                    ans.append(s1[i])
+
+                    i -= 1
+                
+                else:
+
+                    ans.append(s2[j])
+
+                    j -= 1
         
-        while i > 0:
+        while i >= 0:
 
-            superSeq.append(s1[i - 1])
+            ans.append(s1[i])
 
             i -= 1
         
-        while j > 0:
+        while j >= 0:
 
-            superSeq.append(s2[j - 1])
+            ans.append(s2[j])
 
             j -= 1
-        
-        sCS = "".join(superSeq[::-1])
 
-        return sCS
+        res = "".join(ans)[::-1]
+
+        return res
+        
