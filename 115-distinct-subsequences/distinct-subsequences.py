2becdc4b1f38c67395a81694f8acc883ab2e9124
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-08-30 11:37:21 +0530

    Time: 498 ms (36.77%) | Memory: 74.8 MB (36.21%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,42 +1,6 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
-        cnt = 0
-
-        def getDistCnt(m,n):
-
-            if n == 0:
-
-                return 1
-
-            if m == 0 :
-                
-                if n == 0:
-
-                    return 1
-                
-                return 0
-            
-            if dp[m][n] != -1:
-
-                return dp[m][n]
-            
-            if s1[m-1] == s2[n-1]:
-
-                take_cnt =  getDistCnt(m-1,n-1)
-
-                not_take_cnt = getDistCnt(m-1,n)
-
-                dp[m][n] = take_cnt + not_take_cnt
-            
-            else:
-
-                # dont take s1 char
-
-                dp[m][n] = getDistCnt(m-1,n)
-            
-            return dp[m][n]
-        
         s1 = s
 
         s2 = t
@@ -47,4 +11,30 @@ class Solution:
 
         dp = [ [-1 for i in range(n+1)] for j in range(m+1)]
 
-        return getDistCnt(m,n)
+        for i in range(m+1):
+
+            dp[i][0] = 1
+        
+        for j in range(n+1):
+
+            dp[0][j] = 0
+        
+        dp[0][0] = 1
+
+        for i in range(1,m+1):
+
+            for j in range(1,n+1):
+
+                if s1[i-1] == s2[j-1]:
+
+                    take_cnt =  dp[i-1][j-1]
+
+                    not_take_cnt = dp[i-1][j]
+
+                    dp[i][j] = take_cnt + not_take_cnt
+                
+                else:
+
+                    dp[i][j] = dp[i-1][j]
+
+        return dp[m][n]
