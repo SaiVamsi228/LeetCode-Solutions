5312297f38e8ab4acfa3d56ca2ec28928e85647b
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-09-09 20:04:08 +0530

    Time: 1148 ms (15.97%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,24 +1,11 @@
-
-
-
-SELECT comb.student_id, comb.student_name, comb.subject_name, IFNULL(cnt_frq.attended_exams, 0) AS attended_exams
+-- # Write your MySQL query statement below
+SELECT comb.student_id, comb.student_name, comb.subject_name, COUNT(E.subject_name) AS attended_exams
 FROM 
-
-(SELECT s.student_id, s.student_name, sb.subject_name
-FROM Students s
-CROSS JOIN Subjects sb
-ORDER BY student_id, student_name, subject_name) AS comb
-
+(SELECT student_id, student_name, subject_name
+FROM Students CROSS JOIN
+Subjects) AS comb
 LEFT JOIN
-
-(SELECT student_id, subject_name, COUNT(subject_name) as attended_exams
-FROM Examinations
-GROUP BY student_id,subject_name
-ORDER BY student_id) AS cnt_frq
-
-ON comb.student_id = cnt_frq.student_id AND comb.subject_name = cnt_frq.subject_name
+Examinations E
+ON comb.student_id = E.student_id AND comb.subject_name = E.subject_name
 GROUP BY comb.student_id, comb.student_name, comb.subject_name
-ORDER BY comb.student_id, comb.student_name, comb.subject_name;
-
-
-
+ORDER BY student_id, subject_name;
