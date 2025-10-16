ff1fac6dfe84bd2994784b882667599fa1e3558a
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-10-16 14:55:34 +0530

    Time: 7 ms (55.90%) | Memory: 19.3 MB (38.17%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,51 +1,51 @@
 class Solution:
-    def getList(self,mat,n):
-
-        graph = [[] for i in range(n+1)]
-
-        for i in range(n):
-
-            for j in range(n):
-
-                if i==j or mat[i][j] == 0:
-
-                    continue
-                
-                else:
-
-                    graph[i+1].append(j+1)
-        
-        return graph
-    
-    def dfs(self,v,adj,visited):
-
-        for neighbour in adj[v]:
-            
-            if neighbour not in visited:
-
-                visited.add(neighbour)
-
-                self.dfs(neighbour, adj, visited)
-        return
-
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         
-        n = len(isConnected)
+        def getGraph(matrix):
 
-        adj = self.getList(isConnected,n)
+            n = len(matrix)
 
+            adj = [[] for i in range(n)]
+
+            for i in range(n):
+
+                for j in range(n):
+
+                    if matrix[i][j] == 1:
+
+                        adj[i].append(j)
+
+                        adj[j].append(i)
+            
+            return adj
+        
+        adj = getGraph(isConnected)
+
+        n = len(adj)
+
+        def dfs(node):
+
+            if visited[node]:
+
+                return 
+            
+            visited[node] = True
+
+            for neighbour in adj[node]:
+
+                dfs(neighbour)
+        
         cnt = 0
+        
+        visited = [False for i in range(n)]
 
-        visited = set()
+        for node in range(n):
 
-        for i in range(1,n+1):
+            if not visited[node]:
 
-            if i not in visited:
-
-                visited.add(i)
-
-                self.dfs(i,adj,visited)
+                dfs(node)
 
                 cnt += 1
         
         return cnt
+                    
