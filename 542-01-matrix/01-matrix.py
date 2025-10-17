c1ece5ab2a524b99984a66aef4ffb6d17ee1984a
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-10-17 08:36:10 +0530

    Time: 239 ms (13.18%) | Memory: 21 MB (36.65%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,20 +1,12 @@
+from collections import deque
 class Solution:
-    def isValid(self, r, c, m , n):
-
-        if  0 <= r < m and 0 <= c < n :
-
-            return True
-        
-        return False
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         
-        q = []
+        q = deque()
 
         m = len(mat)
 
         n = len(mat[0])
-
-        dist = [ [2**31 + 1 for j in range(n)] for i in range(m)]
 
         for i in range(m):
 
@@ -23,38 +15,53 @@ class Solution:
                 if mat[i][j] == 0:
 
                     q.append((i,j))
+        
+        dist = [[ float('inf') for i in range(n)] for j in range(m)]
 
-                    dist[i][j] = 0
+        def isValid(r,c,m,n):
 
-        d = 1
-
+            return (0<=r<m and 0<=c<n)
+        
         dr = [0,0,1,-1]
 
         dc = [1,-1,0,0]
-        
-        while q :
 
-            n_ = len(q)
+        dis = 0
 
-            for _ in range(n_):
+        while q:
 
-                r,c = q.pop(0)
+            t = len(q)
+
+            dis += 1
+
+            for _ in range(t):
+
+                r,c = q.popleft()
 
                 for i in range(4):
 
-                    nr, nc = r + dr[i] , c + dc[i]
+                    nr,nc = r + dr[i], c + dc[i]
 
-                    if self.isValid(nr,nc,m,n):
+                    if isValid(nr,nc,m,n):
 
-                        if mat[nr][nc] == 1 and  d < dist[nr][nc]:
+                        if mat[nr][nc] == 1:
 
-                            dist[nr][nc] = d
+                            if dis < dist[nr][nc] :
 
-                            q.append((nr,nc))
+                                dist[nr][nc] = dis
+
+                                q.append((nr,nc))
+        
+        for i in range(m):
+
+            for j in range(n):
+
+                if dist[i][j] == float('inf'):
+
+                    dist[i][j] = 0
+
+        return dist
                 
-            
-            d += 1
+                            
 
-        mat = dist
 
-        return mat
