fe2a27f7bba00bcf488858e8c58d1cc4fae5fd5a
Sai Vamsi Sirikonda <126693735+SaiVamsi228@users.noreply.github.com>
2025-12-17 10:59:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (11.84%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -1,61 +1,27 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-
+    def reorderList(self, head):
         if not head or not head.next:
-            
-            return head
-            
-        arr = []
-
-        temp = head
-
-        while temp :
-
-            arr.append(temp)
-
-            temp = temp.next
+            return
         
-        n = len(arr)
-
-        left, right = 0, n - 1
-
-        flag = True
-
-        new_head = arr[left]
-
-        while left < right:
-
-            if flag:
-
-                arr[left].next = arr[right]
-
-                left += 1
-            
-                flag = False
-
-                last = arr[right]
-            
-            else:
-
-                arr[right].next = arr[left]
-
-                right -= 1
-
-                flag = True
-
-                last = arr[left]
-
-        last.next = None
-
-        return new_head
-
-
+        # Step 1: Find the middle of the list
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
         
+        # Step 2: Reverse the second half
+        prev, curr = None, slow.next
+        slow.next = None  # Disconnect the two halves
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
+        # Step 3: Merge the two halves
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
